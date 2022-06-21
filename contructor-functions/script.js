c9f764b13b49016edcb501e2b264a76101be9552
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-21 08:54:52 -0300

    c

--- contructor-functions/script.js
+++ contructor-functions/script.js
@@ -108,3 +108,31 @@ li.offsetLeft;
 li.click();
 // Qual o construtor do dado abaixo:
 li.hidden.constructor.name
+
+
+//Retorne um número aleatorio entre 1050 e 2000
+const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050)
+console.log(aleatorio)
+
+//Retorne o maior número da lista abaixo
+const maiorNumero = '4, 5, 200, 8, 9'
+const arrayNumero = maiorNumero.split(', ')
+const numeroMaximo = Math.max(...arrayNumero)
+
+//Crie uma funcao para limpar os precos e retornar os numeros com centavos arredondados depois retorne a soma total
+
+const listaPrecos =  ['R$ 59,99', 'R$100,222', 'R$230', 'R$200']
+
+function limparPreco(preco) {
+  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',','.')
+  preco = +preco.toFixed(2)
+  return preco
+}
+
+let soma = 0
+listaPrecos.forEach((preco) => {
+  soma += limparPreco(preco)
+})
+
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+limparPreco(listaPrecos[0])
