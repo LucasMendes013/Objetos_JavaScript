1271405888f1c5bd049d01361af2ee4ba46a6d79
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-22 11:41:02 -0300

    c

---
+++ Array_Interacao/script-aula.js
@@ -0,0 +1,17 @@
+//[].forEach()
+//[].forEach(callback(itemAtual, index, array)) a função de callback é executada para cada item da array. Ela possui três argumentos, itemAtual (valor do item da array), index (index do valor na array) e array (array original).
+
+  const carros = ['Ford', 'Fiat', 'Honda'];
+  carros.forEach(function(item, index, array) {
+  console.log(item.toUpperCase());
+  });
+  // com Arrow Function
+  carros.forEach((item, index, array) => {
+  console.log(item.toUpperCase());
+  });
+//O método sempre retorna undefined
+
+
+
+
+
